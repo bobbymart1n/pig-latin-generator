46376238dbe00bfb0fbf92dc049f7c89dba59466
Bobby Martin and Cory West-Forbes <cory@>
2018-03-28 08:56:26 -0700

    Add generator function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+var vowels = ["a", "e", "i", "o", "u"];
+var pigLatinArray = [];
+var generator = function(str) {
+  var arrayOfWords = str.split(' ');
+  for (var i = 0; i < arrayOfWords.length; i++) {
+   for (var j = 0; j < vowels.length; j++) {
+     if(arrayOfWords[i].charAt(0) === vowels[j]) {
+       pigLatinArray.push(arrayOfWords[i].concat("way"));
+     }
+   }
+  }
+  return pigLatinArray;
+}
 $(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#userInput").val();
-    console.log(userInput);
+    var userInput = $("#userInput").val().toLowerCase();
   });
 });
