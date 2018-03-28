c6aa496eb6772248d7f2de0be2a34e5d15dfba87
Bobby Martin and Cory West-Forbes <bobby@>
2018-03-28 09:21:30 -0700

    Add double consonant spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ var pigLatinArray = [];
 var generator = function(str) {
   var arrayOfWords = str.split(' ');
   for (var i = 0; i < arrayOfWords.length; i++) {
+    if(arrayOfWords[i].charAt(0) === arrayOfWords[i].charAt(1)) {
+      var sliceDoubleConsonant = arrayOfWords[i].slice(2);
+      var addPigLatinEnding = arrayOfWords[i].slice(0, 2);
+      pigLatinArray.push(sliceDoubleConsonant.concat(addPigLatinEnding + 'ay'))
+    }
    for (var j = 0; j < vowels.length; j++) {
      if(arrayOfWords[i].charAt(0) === vowels[j]) {
        pigLatinArray.push(arrayOfWords[i].concat("way"));
